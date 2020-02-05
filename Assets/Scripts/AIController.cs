7424eb8932f7252bec62f19d417445e9a5b8b506
Pål Grønstad Solheim <paul.solheim@gmail.com>
2020-02-05 16:06:13 +0100

    før 7.gang
    klargjort til avoidance koding.

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -13,11 +13,10 @@ public class AIController : MonoBehaviour
     Vector3 nextTarget;
     int currentWP = 0;
     float totalDistanceToTarget;
-    bool isJump = false;
 
     GameObject tracker;
     int currentTrackerWP = 0;
-    float lookAhead = 10;
+    public float lookAhead = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +28,7 @@ public class AIController : MonoBehaviour
 
         tracker = GameObject.CreatePrimitive(PrimitiveType.Capsule);
         DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
         tracker.transform.position = ds.rb.gameObject.transform.position;
         tracker.transform.rotation = ds.rb.gameObject.transform.rotation;
     }
@@ -56,68 +56,23 @@ public class AIController : MonoBehaviour
     {
         ProgressTracker();
         Vector3 localTarget = ds.rb.gameObject.transform.InverseTransformPoint(tracker.transform.position);
-        Vector3 nextLocalTarget = ds.rb.gameObject.transform.InverseTransformPoint(nextTarget);
-        float distanceToTarget = Vector3.Distance(target, ds.rb.gameObject.transform.position);
-
         float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-        float nextTargetAngle = Mathf.Atan2(nextLocalTarget.x, nextLocalTarget.z) * Mathf.Rad2Deg;
-
         float steer = Mathf.Clamp(targetAngle * steeringSensitivity, -1, 1) * Mathf.Sign(ds.CurrentSpeed);
 
-        float distanceFactor = distanceToTarget / totalDistanceToTarget;
         float speedFactor = ds.CurrentSpeed / ds.maxSpeed;
-        float nextWaypointFactor = Mathf.Abs(nextTargetAngle) / 180;
 
-        float accel = Mathf.Lerp(accellerationSensitivity, 1, distanceFactor);
-        float brake = Mathf.Lerp(-1 + (nextWaypointFactor * brakingSensitivity), 1 + speedFactor, 1 - distanceFactor);
+        float corner = Mathf.Clamp(Mathf.Abs(targetAngle), 0, 90);
+        float cornerFactor = corner / 90.0f;
 
-        // Next some hardcoded rules (trial and error based):
+        float brake = 0;
+        if (corner > 10 && speedFactor > 0.1f)
+            brake = Mathf.Lerp(0, 1 + speedFactor * brakingSensitivity, cornerFactor);
 
-        if (speedFactor < 0.1f) { brake = 0f; }     // If pointing wrong way after flipping
-
-        if (nextTargetAngle > 20)
-        {
-            if (speedFactor > 0.2f)
-            {
-                brake += 0.2f;
-                accel -= 0.2f;
-            }
-        }
-
-        if (isJump)
-        {
-            accel = 1;
-            brake = 0;
-            Debug.Log("isJump");
-        }
-
-        // Debug.Log("Brake:" + brake + " Accel: " + accel + " Speed: " + ds.rb.velocity.magnitude);
-
-        // if (distanceToTarget < 5) { brake = 1.0f; accel = 0.1f; }
+        float accel = 1f;
+        if (corner > 20 && speedFactor > 0.2f)
+            accel = Mathf.Lerp(0, 1 * accellerationSensitivity, 1 - cornerFactor);
 
         ds.Go(accel, steer, brake);
-
-        if (distanceToTarget < 4) //threshold, make larger if car starts to circle waypoint
-        {
-            currentWP++;
-            if (currentWP >= circuit.waypoints.Length)
-                currentWP = 0;
-            target = circuit.waypoints[currentWP].transform.position;
-
-            if (currentWP == circuit.waypoints.Length - 1)
-                nextTarget = circuit.waypoints[0].transform.position;
-            else
-                nextTarget = circuit.waypoints[currentWP + 1].transform.position;
-
-            totalDistanceToTarget = Vector3.Distance(target, ds.rb.gameObject.transform.position);
-
-            if (ds.rb.gameObject.transform.InverseTransformPoint(target).y > 4)
-            {
-                isJump = true;
-            }
-            else isJump = false;
-
-        }
 
         ds.CheckForSkid();
         ds.CalculateEngineSound();
